e2bef085e9177666abd6d7469fc87a8cce8dba01
Gerardo Chinchilla <00049518@uca.edu.sv>
2022-10-16 19:10:05 -0600

    separando productos en componentes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 
 import './App.css';
 import './components/Api/RestProducts'
-import React, {useState, useEffect } from 'react';
 import RestProducts from './components/Api/RestProducts';
 
 function App() {
-  const urlProducts = 'https://private-894052-starproviders.apiary-mock.com/products'
-  const [products, setProducts] = useState()
-
-  const fetchApi = ()=> {
-    fetch(urlProducts).then((res)=>(
-      res.json().then((resJson)=>(
-        setProducts(resJson)
-        )
-      )
-    ))
-  }
-  useEffect(()=>{
-    fetchApi()
-  }, []);
-  return (<RestProducts></RestProducts>);
+  return (
+    <div className="App">
+      <RestProducts />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Api/RestProducts.js
+++ src/components/Api/RestProducts.js
@@ -1,6 +1,6 @@
-
 import '../../App.css';
 import React, {useState, useEffect } from 'react';
+import { Product } from '../Productos';
 
 function RestProducts() {
   const urlProducts = 'https://private-894052-starproviders.apiary-mock.com/products'
@@ -21,22 +21,7 @@ function RestProducts() {
     <div className='container'>
       <div className='row'>
         {!products ? 'Cargando...' : 
-          products.map((product, index)=>{
-            return(
-              <div key={index} className='col-md-4 box-shadow'>
-                <button className='card mb-4 box-shadow'>
-                  <div className='card-body'> 
-                    <h5 className='mb-1'>{product.name}</h5>
-               
-                    <p className='mb-1'>{product.description}</p>
-                    <p className='mb-1'>{product.price}</p>
-                    <p className='mb-1'>{product.provider}</p>
-                    <p className='mb-1'>{product.category}</p>
-                  </div>
-                </button>
-              </div>
-            )
-          })
+          products.map((product)=> <Product key={product.code} product={product}/>)
         }
       </div>
     </div>

b6e0f0a5f8de22418a4210474da6c6173a494459
Gerardo Chinchilla <00049518@uca.edu.sv>
2022-10-19 21:58:24 -0600

    Eduardo

--- src/App.js
+++ src/App.js
@@ -9,18 +9,18 @@ function App() {
 
 // <Login/>
   return (
-    <div>
+    <div className='App'>
+      <NavBar 
+        UserName = 'Usuario'
+      />
       <Header 
         Name = 'Star Providers'
         opc1='Boton 1'
         opc2='Boton 2'
         opc3='Boton 3'
       />
-      <NavBar 
-        UserName = 'Usuario'
-      />
       <RestProducts/>
-      <Footer />
+      {/* <Footer /> */}
     </div>
 
     );

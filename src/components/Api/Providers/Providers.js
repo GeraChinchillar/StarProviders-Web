41f3cbadcf26731669b4d3344fb1cc363151dcd8
DianaSanchez1103 <54149040+DianaSanchez1103@users.noreply.github.com>
2022-11-21 18:10:48 -0600

    visual changes

--- src/components/Api/Providers/Providers.js
+++ src/components/Api/Providers/Providers.js
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
-import Modales from "../../commons/Modales";
 import '../../../App.css';
+import Modal from 'react-bootstrap/Modal';
 
 export const Providers = (props) => {
-  const [isOpen, setIsOpen] = useState(false)
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   const { provider } = props
   console.log({ provider });
 
   return (
     <div className='col-md-3 box-shadow oneProduct'>
-      <button className='colorManual' variant="primary" onClick={() => setIsOpen(true)}>
+      <button className='colorManual' variant="primary" onClick={handleShow}>
       <div className='card-body'>
           <img src={provider.urlLogo} className='provider-img' alt='product'></img>
           <h4 className='card-title-product'>{provider.name}</h4>
@@ -17,12 +19,17 @@ export const Providers = (props) => {
         </div>
       </button>
       <div>
-        <Modales open={isOpen} onClose={() => setIsOpen(false)} title={provider.name}>
-          <img className='card-img provider-img' src={provider.urlLogo} alt='providers'/>
-          <a href={provider.site} className='provider-link'>Página de Walmart</a>
-          <a href={provider.urlLocation} className='provider-link'>Ubicación</a>
-          <p className='card-text-product'><b>Ubicación:</b> {provider.cityName}</p>
-        </Modales>
+        <Modal className='modal-provider modal-dialog-centered modal-md' show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+              <Modal.Title>{provider.name}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <img className='card-img-p ' src={provider.urlLogo} alt='providers'/>
+            <p><a href={provider.site} className='provider-link'>Página de {provider.name}</a></p>
+            <p><a href={provider.urlLocation} className='provider-link'>Ubicación</a></p>
+            <p className='card-text-provider'><b>Ubicación:</b> {provider.cityName}</p>
+          </Modal.Body>
+        </Modal>
       </div>
     </div>
   )
